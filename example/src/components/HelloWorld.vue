5233ce105ceb3ab3cb2ad93e9c94ec567637ca41
frncs1999 <francis.resugenio@davidvenneit.com>
2020-09-11 04:20:09 +0800

    quiz

--- example/src/components/HelloWorld.vue
+++ example/src/components/HelloWorld.vue
@@ -1,42 +1,31 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">Cras justo odio</li>
+      <li class="list-group-item">Dapibus ac facilisis in</li>
+      <li class="list-group-item">Morbi leo risus</li>
+      <li class="list-group-item">Porta ac consectetur ac</li>
+      <li class="list-group-item">Vestibulum at eros</li>
     </ul>
   </div>
 </template>
 
 <script>
+const axios = require('axios');
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  created() {
+    axios.get("https://reddit.com/r/aww.json")
+      .then(response => {
+        console.log(response.data.data.children)
+      })
+      .catch(error => {
+        console.log(error)
+      })
   }
 }
 </script>
@@ -57,4 +46,9 @@ li {
 a {
   color: #42b983;
 }
+.list-group-flush{border-radius:0}
+.list-group-flush>.list-group-item{border-width:0 0 1px}
+
+.list-group-item{position:relative;display:block;padding:.75rem 1.25rem;background-color:#fff;border:1px solid rgba(0,0,0,.125)}
+.list-group{display:-ms-flexbox;display:flex;-ms-flex-direction:column;flex-direction:column;padding-left:0;margin-bottom:0;border-radius:.25rem}
 </style>
---
+++ quiz/src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <ul class="nav">
+        <li class="nav-item">
+            <b><a class="nav-link disabled" href="#">Fancy Quiz App</a></b>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link disabled">Counter: {{ numCorrect }}/{{ numTotal }}</a>
+        </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [
+    'numCorrect',
+    'numTotal'
+  ]
+}
+</script>
